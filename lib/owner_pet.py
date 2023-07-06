5ac4b7dc3443f6ed75af85b4b8d54990cd98e6f2
Marc Katz <marc.f.katz@gmail.com>
2023-07-06 12:00:07 -0400

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -4,12 +4,19 @@ class Pet:
 
     def __init__(self, name, pet_type, owner=None) -> None:
         self.name = name
-        if pet_type in Pet.PET_TYPES:
-            self.pet_type = pet_type
-        else:
-            raise Exception()
+        self.pet_type = pet_type
         self.owner = owner
         Pet.all.append(self)
+
+    def get_pet_type(self):
+        return self._pet_type
+    def set_pet_type(self, pet_type):
+        if pet_type in Pet.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception()
+    pet_type = property(get_pet_type, set_pet_type)
+
 
     def __repr__(self):
         return self.name
