a0e496ce8ec46a64df81181cd8aa05cd1528db1e
Marc Katz <marc.f.katz@gmail.com>
2023-07-06 09:35:25 -0400

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None) -> None:
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception()
+        self.owner = owner
+        Pet.all.append(self)
+
+    def __repr__(self):
+        return self.name
+
 
 class Owner:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+        self.pet_list = []
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception()
+        
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
